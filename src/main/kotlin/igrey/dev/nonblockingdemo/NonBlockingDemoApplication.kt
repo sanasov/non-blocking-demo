e4a6fc6b159c91476e26cf45586220025c8a7533
ssanasov <ssanasov@domclick.ru>
2021-09-22 11:39:00 +0300

    refactoring

--- src/main/kotlin/igrey/dev/nonblockingdemo/NonBlockingDemoApplication.kt
+++ src/main/kotlin/igrey/dev/nonblockingdemo/NonBlockingDemoApplication.kt
@@ -8,5 +8,4 @@ class NonBlockingDemoApplication
 
 fun main(args: Array<String>) {
     runApplication<NonBlockingDemoApplication>(*args)
-    Thread.getAllStackTraces().keys.sortedBy { it.name }.forEach { println(it.name) }
 }
