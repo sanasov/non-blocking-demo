b3b0d8dc3cdf47f05c42307258493bbac0a7d609
ssanasov <ssanasov@domclick.ru>
2021-10-05 18:12:13 +0300

    refactoring

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,7 +18,9 @@ repositories {
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-webflux")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+
     implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
+
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 
